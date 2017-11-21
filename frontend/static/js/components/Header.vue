80f09bf795ac5bfbb2ad278d91cda94c4094bfc2
Smoothie <smoothie@smoothie.digital>
2017-11-21 10:28:55 +0300

    корзина

--- frontend/static/js/components/Header.vue
+++ frontend/static/js/components/Header.vue
@@ -27,6 +27,7 @@
           <div class="header__right-search_right">
             <router-link tag="button" :to="{name: 'basket'}" class="button">Корзина <span v-for='item in basket'>({{ item.total_count }})</span></router-link>
           </div>
+
         </div>
       </div>
     </div>
