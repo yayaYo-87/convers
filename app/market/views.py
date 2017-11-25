9bae50b41ea94304e3915975ce1e93cfe380ed0d
Gennady Voytko <g.voytko@yandex.ru>
2017-11-25 11:54:18 +0300

    Update views.py

--- app/market/views.py
+++ app/market/views.py
@@ -63,7 +63,7 @@ def resend_pay(request):
     return HttpResponse(f.content)
 
 
-@require_http_methods(["POST"])
+# @require_http_methods(["POST"])
 # @csrf_exempt
 def shiptorg(request):
     json_data = json.loads(request.body.decode("utf-8"))['json']
