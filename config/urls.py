ca7709c01815e1f332750ccadc4d7919f6193545
Smoothie <smoothie@smoothie.digital>
2017-11-26 11:35:55 +0300

    csrf

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from config.api import router
+from django.views.decorators.csrf import csrf_exempt
 
 from app.market.views import IndexView, init_pay, cancel_pay, get_state_pay, resend_pay, shiptorg
 from config import settings
@@ -28,7 +29,7 @@ urlpatterns = [
     url(r'^cancel_pay/$', cancel_pay, name='cancel_pay'),
     url(r'^get_state_pay/$', get_state_pay, name='get_state_pay'),
     url(r'^resend_pay/$', resend_pay, name='resend_pay'),
-    url(r'^shiptorg/$', shiptorg, name='shiptorg'),
+    url(r'^shiptorg/$', csrf_exempt(shiptorg), name='shiptorg'),
     url(r'^[a-z]*_?/?[a-z]*_?/?[0-9]*_?/?[0-9]*$', IndexView.as_view(), name='index'),
     # url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 
