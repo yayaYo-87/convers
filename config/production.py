d0c242dfa71672d162ce8af2ee16444d7c1948d2
Gennady Voytko <g.voytko@yandex.ru>
2018-10-03 18:50:33 +0300

    Update production.py

--- config/production.py
+++ config/production.py
@@ -10,7 +10,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'convers',
-        'PASSWORD': 1111,
+        'PASSWORD': DATABASE_PASSWORD,
         'HOST': 'localhost',
         'USER': 'postgres',
     }
